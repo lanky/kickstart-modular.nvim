e3e17738835a8f3d4eb32bb390ed1c4d9de33cd7
Stuart Sears <stuart@sjsears.com>
2024-11-13 22:06:35 +0000

    add a number of interesting colour schemes

---
+++ lua/custom/plugins/colorschemes.lua
@@ -0,0 +1,10 @@
+return {
+  "rockerBOO/boo-colorscheme-nvim",
+  "Mofiqul/vscode.nvim",
+  "flazz/vim-colorschemes",
+  "olimorris/onedarkpro.nvim",
+  {
+    "RishabhRD/nvim-rdark",
+    dependencies = { "tjdevries/colorbuddy.nvim" },
+  },
+}

0257b6ccb058ee8b0319ce38e7e01ef1ca08b8cf
Stuart Sears <stuart@sjsears.com>
2024-11-13 22:09:22 +0000

    toggle terminal

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,21 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 20
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+      end
+    end,
+    open_mapping = [[<c-\>]],
+    hide_numbers = true,
+    start_in_insert = true,
+    close_on_exit = true,
+    shell = vim.o.shell,
+    persist_size = true,
+    direction = "horizontal",
+    insert_mappings = true,
+  },
+}

7a0c09bfd0d96e289a160f3acaf7ab4ed738d665
Stuart Sears <stuart@sjsears.com>
2024-11-19 13:48:40 +0000

    Add glow to Mason autoinstall list

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -238,6 +238,7 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         "stylua", -- Used to format Lua code
+        "glow", -- markdown previewer
       })
       require("mason-tool-installer").setup { ensure_installed = ensure_installed }
 
